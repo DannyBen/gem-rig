b367ee2a37d0df760e6285ad90b765ba0ee02f72
Danny Ben Shitrit <dannyb@telesofia.com>
2018-03-11 21:52:37 +0000

    fixes

--- bin=yes/lib/%{gem_name}.rb
+++ bin=yes/lib/%{gem_name}.rb
@@ -1,2 +1,7 @@
-require '%{gem_name}/#{class_name}'
+require '%{gem_name}/%{class_name}'
 require '%{gem_name}/command_line'
+
+module %{gem_name_camel}
+  module Commands
+  end
+end
--- bin=yes/lib/%{gem_name}/commands/%{command_name}.rb
+++ bin=yes/lib/%{gem_name}/commands/%{command_name}.rb
@@ -1,4 +1,4 @@
-module %{gem_name_camel}::
+module %{gem_name_camel}::Commands
   module %{command_name_camel}
 
     def %{command_name}
